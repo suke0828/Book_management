fe90eff18d1bd68f396ae2e3446e28b8dab9a762
suke <mwbspa@gmail.com>
2021-05-30 14:38:14 +0900

    example名修正 #7

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  it 'isbnがある場合、有効である' do
+  it 'isbnがある場合、有効であること' do
     book = FactoryBot.create(:book)
     expect(book).to be_valid
   end
 
-  it 'isbnが無い場合、無効である' do
+  it 'isbnが無い場合、無効であること' do
     book = described_class.new(isbn: nil)
     book.valid?
     expect(book.errors[:isbn]).to include("can't be blank")
