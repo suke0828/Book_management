5d7b206906c8ce9471b5f65eb556148027d71bbf
suke <mwbspa@gmail.com>
2021-06-01 11:58:49 +0900

    new,createアクション作成 #9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,9 +7,24 @@ class BooksController < ApplicationController
              end
   end
 
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book_info = book.get_json(book.isbn)
+    openbd_book = Book.new(book_info)
+    redirect_to root_path if openbd_book.save
+  end
+
   private
 
   def query
     @query ||= params[:query]
+  end
+
+  def book_params
+    params.require(:book).permit(:isbn)
   end
 end
