2b7285d6fe56eea15093914003efbc21bbe85867
suke <mwbspa@gmail.com>
2021-06-01 12:01:17 +0900

    new,createのテスト作成 #9

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -4,7 +4,52 @@ RSpec.describe 'Books', type: :request do
   describe 'GET /index' do
     it '正常にレスポンスをを返すこと' do
       get root_path
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:ok)
     end
+  end
+
+  describe 'GET /new' do
+    it '正常にレスポンスをを返すこと' do
+      get new_book_path
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'POST /create' do
+    context 'パラメータが正常な場合' do
+      it '正常にレスポンスをを返すこと' do
+        post books_path, params: { book: { isbn: 9_784_797_395_235 } }
+        expect(response).to have_http_status(:found)
+      end
+
+      it '書籍情報が登録されること' do
+        expect do
+          post books_path, params: { book: { isbn: 9_784_797_395_235 } }
+        end.to change(Book, :count).by(1)
+      end
+
+      it 'リダイレクトすること' do
+        post books_path, params: { book: { isbn: 9_784_797_395_235 } }
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    # context 'パラメータが不正な場合' do
+    #   it 'リクエストが成功すること' do
+    #     post books_path, params: { book: { isbn: 0 } }
+    #     expect(response.status).to eq have_http_status(:ok)
+    #   end
+
+    #   it '書籍情報が登録されないこと' do
+    #     expect do
+    #       post books_path, params: { book: { isbn: 0 } }
+    #     end.not_to change(Book, :count)
+    #   end
+
+    #   it 'エラーが表示されること' do
+    #     post books_path, params: { book: { isbn: 0 } }
+    #     expect(response.body).to include 'undefined method'
+    #   end
+    # end
   end
 end
