d4021942ac610284b86d326e8f8d2ecc655f0017
suke <mwbspa@gmail.com>
2021-06-02 19:38:25 +0900

    APIを受け取る処理を作成 #11

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,9 +13,16 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book_info = book.get_json(book.isbn)
-    openbd_book = Book.new(book_info)
-    redirect_to root_path if openbd_book.save
+    json_api_data = call_openbd_api(book.isbn)
+    if error_call_api(json_api_data)
+      redirect_to new_book_path
+    else
+      Book.create(
+        get_json(json_api_data)
+      )
+      response_success(:book, :create)
+      redirect_to root_path
+    end
   end
 
   private
@@ -26,5 +33,31 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:isbn)
+  end
+
+  def get_json(json_api_data)
+    # 呼び出しが成功した場合、必要な情報だけを抜き出して新しいJSONを作成する
+    {
+      isbn: json_api_data[0]['summary']['isbn'],
+      title: json_api_data[0]['summary']['title'],
+      publisher: json_api_data[0]['summary']['publisher'],
+      pubdate: json_api_data[0]['summary']['pubdate'],
+      cover: json_api_data[0]['summary']['cover'],
+      author: json_api_data[0]['summary']['author']
+    }
+  end
+
+  def call_openbd_api(isbn)
+    url = "https://api.openbd.jp/v1/get?isbn=#{isbn}"
+    uri = URI.parse(url)
+    response = Net::HTTP.get(uri)
+    JSON.parse(response)
+  end
+
+  def error_call_api(json_api_data)
+    # 呼び出しが失敗した場合
+    response_internal_server_error if json_api_data.nil?
+    # 検索結果が0だった場合
+    response_internal_server_error if json_api_data[0].nil?
   end
 end
