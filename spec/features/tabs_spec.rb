e48aa6cc1e27ba58d9d6947cb387de8335e79c90
suke <mwbspa@gmail.com>
2021-06-14 11:09:55 +0900

    タブのテスト #23

--- spec/features/tabs_spec.rb
+++ spec/features/tabs_spec.rb
@@ -7,11 +7,17 @@ RSpec.describe 'タブのページ遷移', type: :feature do
 
   it '書籍一覧画面に遷移すること' do
     click_link '書籍一覧'
-    expect(page).to have_current_path books_path
+    have_selector('tab_index', class: 'is-active')
+    have_no_selector('tab_new', class: 'is-active')
+    have_current_path(books_path)
+    expect(page).to have_selector('table', class: 'table')
   end
 
   it '書籍登録画面に遷移すること' do
+    have_selector('tab_mew', class: 'is-active')
+    have_no_selector('tab_index', class: 'is-active')
     click_link '書籍追加'
-    expect(page).to have_current_path new_book_path
+    have_current_path(new_book_path)
+    expect(page).to have_selector('h1', text: '書籍登録')
   end
 end
