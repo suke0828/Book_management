159704e4cc4f47e9f8f74c14fe89975974047540
suke <mwbspa@gmail.com>
2021-06-29 11:18:32 +0900

    書籍追加画面のテストの修正 #35

--- spec/features/tabs_spec.rb
+++ spec/features/tabs_spec.rb
@@ -14,10 +14,10 @@ RSpec.describe 'タブのページ遷移', type: :feature do
   end
 
   it '書籍登録画面に遷移すること' do
-    have_selector('tab_mew', class: 'is-active')
+    have_selector('tab_new', class: 'is-active')
     have_no_selector('tab_index', class: 'is-active')
     click_link '書籍追加'
     have_current_path(new_book_path)
-    expect(page).to have_selector('h1', text: '書籍登録')
+    expect(page).to have_selector('h1', text: '書籍追加')
   end
 end
